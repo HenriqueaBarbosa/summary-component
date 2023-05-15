f0c8816a849d32d9783fd209e93d4d3e17c01a30
Henrique Barbosa <henrique.abdiel@gmail.com>
2023-05-15 04:15:03 -0300

    style: card and circle components

---
+++ src/components/elements/Card.tsx
@@ -0,0 +1,12 @@
+export default function Card() {
+    return (
+        <div className={`
+            flex flex-col
+            items-center justify-center
+            w-72 h-96 rounded-3xl
+            bg-gradient-to-t from-[#342DEB] via-[#5B3EFA] to-[#6340FC]
+        `}>
+
+        </div>
+    )
+}
---
+++ src/components/elements/Circle.tsx
@@ -0,0 +1,12 @@
+export default function Circle() {
+    return (
+        <div className={`
+            flex flex-col
+            items-center justify-center
+            w-36 h-36 rounded-full
+            bg-gradient-to-t from-[#4A27D3] via-[#462BDB] to-[#4530E7]
+        `}>
+
+        </div>
+    )
+}
