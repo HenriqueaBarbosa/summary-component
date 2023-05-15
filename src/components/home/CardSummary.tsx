df25ccf45be1064402b0e908879e31b47709f915
Henrique Barbosa <henrique.abdiel@gmail.com>
2023-05-15 11:41:47 -0300

    refactor: data

--- src/components/home/CardSummary.tsx
+++ src/components/home/CardSummary.tsx
@@ -23,19 +23,19 @@ export default function CardSummary() {
             <Rating memory>
                 <div className="ml-2 mr-1 text-yellow-500"><IconCloudComputing stroke={1} size={22} /></div>
                 <div className="ml-2 font-semibold text-yellow-500">Memory</div>
-                <div className="flex w-full justify-end mr-1 text-[#333] font-semibold text-sm">80</div>
+                <div className="flex w-full justify-end mr-1 text-[#333] font-semibold text-sm">92</div>
                 <div className="text-zinc-400 mr-2 text-xs">/100</div>
             </Rating>
             <Rating verbal>
                 <div className="ml-2 mr-1 text-green-500"><IconUsersGroup stroke={1} size={22} /></div>
                 <div className="ml-2 font-semibold text-green-500">Verbal</div>
-                <div className="flex w-full justify-end mr-1 text-[#333] font-semibold text-sm">80</div>
+                <div className="flex w-full justify-end mr-1 text-[#333] font-semibold text-sm">61</div>
                 <div className="text-zinc-400 mr-2 text-xs">/100</div>
             </Rating>
             <Rating visual>
                 <div className="ml-2 mr-1 text-blue-500"><IconEye stroke={1} size={22} /></div>
                 <div className="ml-2 font-semibold text-blue-500">Visual</div>
-                <div className="flex w-full justify-end mr-1 text-[#333] font-semibold text-sm">80</div>
+                <div className="flex w-full justify-end mr-1 text-[#333] font-semibold text-sm">72</div>
                 <div className="text-zinc-400 mr-2 text-xs">/100</div>
             </Rating>
 
